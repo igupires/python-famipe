811c2724b58d2c3892e3884b1c8ce36cf69e0dfa
Igor Pires <igor.pires@levty.com>
2026-06-11 14:11:08 -0300

    refactor(sync): Update Sync relations to use Anytype object IDs

--- any_2026/sync.py
+++ any_2026/sync.py
@@ -13,7 +13,7 @@ from anytype import CREATE_URL, HEADERS
 
 class Sync:
     def __init__(self, filepath: str, issues:list[Issue], log: list[str], skip_count:int, update_count:int, create_count:int, error_count:int):
-        self.file = File(filepath, name=f"Sync CSV {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        # self.file = File(filepath, name=f"Sync CSV {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
         self.issues = issues
         self.skip_count = skip_count
         self.update_count = update_count
@@ -24,6 +24,7 @@ class Sync:
         
         #Builds paylooad 
         payload = self.__create_payload()
+        print_log(f" < Creating SYNC >")
         
         response = requests.post(CREATE_URL, headers=HEADERS, json=payload)
         response.raise_for_status()
@@ -42,13 +43,14 @@ class Sync:
             "name": f"Sync of {self.date_time}",
             "properties": [
                 # Note: The format (text, select, etc.) depends on your Anytype Relation definitions
-                {"key": "jiras", "format": "objects", "objects": self.issues if self.tester else []},
-                {"key": "log", "format": "text", "text": "\n".join(self.log)},
-                {"key": "created", "format": "number", "text": self.create_count},
-                {"key": "updated", "format": "number", "text": self.update_count},
-                {"key": "errors", "format": "number", "text": self.error_count},
-                {"key": "skipped", "format": "number", "text": self.skip_count},
-                {"key": "csv_file", "format": "file", "file": [self.file.object_id] if self.file else []}
+                {"key": "bafyreidvgoofi3myciirwbepesaagfcrbzougjj2fx6sg7hwlsjev7xo2q", "format": "objects", "objects": [issue.object_id for issue in self.issues] if self.issues else []},
+                {"key": "bafyreibepvz6te3fagm7uzyikjxxsvcujydja22j3yda52jz7cimvenpwe", "format": "text", "text": """
+""".join(self.log).strip()},
+                {"key": "bafyreibyamo6knxuqpooqulyigsko5ymfyudgfin4i6c42wohblq6nbdne", "format": "number", "number": self.create_count},
+                {"key": "bafyreifw6d4ne7whghs2bd4nhj5aavgr5bkbnj25rvxbyv64zilyj5mlte", "format": "number", "number": self.update_count},
+                {"key": "bafyreifnii7g4cb772galayxlubrg66wqwniby476aytyuuq3nsq7bwzmm", "format": "number", "number": self.error_count},
+                {"key": "bafyreihcgzmkwqyc7i6eirs3mqem2c4vngj72l66n36kwvicvx5k7yv3ku", "format": "number", "number": self.skip_count},
+                # {"key": "csv_file", "format": "file", "file": [self.file.object_id] if self.file else []}
             ]
         }
         
